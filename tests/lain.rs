593f4f5ffb41487937038320ffaa4632cf89e53b
jole <jonas@jole.xyz>
2022-05-27 00:14:00 +0200

    simplifiy test

--- tests/lain.rs
+++ tests/lain.rs
@@ -9,23 +9,22 @@ use jd::dither;
 #[test]
 fn mem() {
     fn ipath(file: &str) -> String {
-        let mut s = String::new();
-        s.push_str(env!("CARGO_MANIFEST_DIR"));
-        s.push_str("/tests/img/");
-        s.push_str(file);
-        s
+        format!("{}/tests/img/{}", env!("CARGO_MANIFEST_DIR"), file)
     }
 
+    // read the source
     let src_jpg = File::open(ipath("lain.jpg")).unwrap();
     let src_ff = Command::new("2ff").stdin(src_jpg).output().unwrap().stdout;
 
+    // generate the dither and write to file
     let mut out = File::create(ipath("lain_out.ff")).unwrap();
-
     dither(&mut Cursor::new(src_ff), &mut out);
 
+    // read in the solution
     let ans_png = File::open(ipath("lain_ans.png")).unwrap();
     let ans_ff = Command::new("2ff").stdin(ans_png).output().unwrap().stdout;
 
+    // read the generated dither
     let mut out_ff = Vec::new();
     File::open(ipath("lain_out.ff"))
         .unwrap()
